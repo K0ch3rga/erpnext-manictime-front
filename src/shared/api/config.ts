961ad8586319517b31a2532299d397f1c22e0bc5
K0ch3rga <mr.skel@mail.ru>
2025-03-24 15:32:47 +0500

    remove auth methods

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,6 +1,1 @@
-import type { AuthVariant } from './erpMethods'
-
 export const erpPath = window.location.protocol + '//' + window.location.host + '/' // 'http://192.168.56.102:8000/'
-const erpLogin = 'Administrator'
-const erpPassword = 'admin'
-export const auth: AuthVariant = { apiKey: 'fe9c7f83128d0b4', apiSecret: 'fd674cf11600677' } // { usr: erpLogin, pwd: erpPassword } не работает из-за same site lax
--- src/shared/api/erpMethods.ts
+++ src/shared/api/erpMethods.ts
@@ -1,115 +1,60 @@
-import { auth, erpPath } from './config'
+import { erpPath } from './config'
 
-const setupErpMetods = (erpPath: string, auth: AuthVariant) => {
-  let authHeader: string
-
-  const checkAuthHeader = async () => {
-    if (!authHeader) {
-      if ('usr' in auth && 'pwd' in auth) {
-        const response = await fetch(`${erpPath}api/method/login?usr=${auth.usr}&pwd=${auth.pwd}`, {
-          headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-          },
-          method: 'POST',
-          body: JSON.stringify(auth),
-          credentials: 'same-origin',
-          mode: 'no-cors',
-        })
-          .then((r) => r.json())
-          .then((r) => r as AuthenticateResponse)
-          .catch(console.log)
-        console.log(response)
-      } else {
-        authHeader = `token ${auth.apiKey}:${auth.apiSecret}`
-        // 'token fe9c7f83128d0b4:fd674cf11600677'
-      }
-    }
-  }
-
-  const getTags = async () => {
-    await checkAuthHeader()
-    return await fetch(`${erpPath}api/method/manictime_integration.api.app.get_tags`, {
-      headers: { Authorization: authHeader },
-    })
-      .then((r) => r.json())
-      .then((r) => r as GetTagsResponse)
-  }
-
-  const getActivities = async (fromDate: Date, toDate?: Date) => {
-    await checkAuthHeader()
-    const query = new URLSearchParams()
-    query.set('from_time', fromDate.toISOString().slice(0, 10))
-    if (toDate) query.set('to_time', toDate.toISOString().slice(0, 10))
-    return await fetch(
-      `${erpPath}api/method/manictime_integration.api.app.get_user_activities?${query.toString()}`,
-      {
-        headers: {
-          Authorization: authHeader,
-        },
-      },
-    )
-      .then((r) => r.json())
-      .then((r) => r as GetActivitiesResponse)
-  }
-
-  const getActivitiesAndUsage = async (fromDate: Date, toDate?: Date) => {
-    await checkAuthHeader()
-    const query = new URLSearchParams()
-    query.set('from_time', fromDate.toISOString().slice(0, 10))
-    if (toDate) query.set('to_time', toDate.toISOString().slice(0, 10))
-    return await fetch(
-      `${erpPath}api/method/manictime_integration.api.app.get_user_usage_and_activities?${query.toString()}`,
-      {
-        headers: {
-          Authorization: authHeader,
-        },
-      },
-    )
-      .then((r) => r.json())
-      .then((r) => r as GetActivitiesResponse)
-  }
-
-  const createActivity = async (
-    newActivity: NewActivityDto,
-    syncId: string,
-    timelineId: string,
-  ) => {
-    await checkAuthHeader()
-    return await fetch(`${erpPath}api/method/manictime_integration.api.app.create_activity`, {
-      headers: {
-        Authorization: authHeader,
-      },
-      body: JSON.stringify({ expectedLastChangeId: syncId, values: newActivity, timelineId }),
-    })
-      .then((r) => r.json())
-      .then((r) => r as GetActivitiesResponse)
-  }
-
-  const updateActivity = async (
-    newActivity: NewActivityDto,
-    syncId: string,
-    timelineId: string,
-    activityId: number,
-  ) => {
-    await checkAuthHeader()
-    return await fetch(`${erpPath}api/method/manictime_integration.api.app.create_activity`, {
-      headers: {
-        Authorization: authHeader,
-      },
-      body: JSON.stringify({
-        expectedLastChangeId: syncId,
-        values: newActivity,
-        timelineId,
-        activityId,
-      }),
-    })
-      .then((r) => r.json())
-      .then((r) => r as GetActivitiesResponse)
-  }
-
-  return [getTags, getActivities, getActivitiesAndUsage, createActivity, updateActivity] as const
+export const getTags = async () => {
+  return await fetch(`${erpPath}api/method/manictime_integration.api.app.get_tags`, {})
+    .then((r) => r.json())
+    .then((r) => r as GetTagsResponse)
 }
+
+export const getActivities = async (fromDate: Date, toDate?: Date) => {
+  const query = new URLSearchParams()
+  query.set('from_time', fromDate.toISOString().slice(0, 10))
+  if (toDate) query.set('to_time', toDate.toISOString().slice(0, 10))
+  return await fetch(
+    `${erpPath}api/method/manictime_integration.api.app.get_user_activities?${query.toString()}`,
+  )
+    .then((r) => r.json())
+    .then((r) => r as GetActivitiesResponse)
+}
+
+export const getActivitiesAndUsage = async (fromDate: Date, toDate?: Date) => {
+  const query = new URLSearchParams()
+  query.set('from_time', fromDate.toISOString().slice(0, 10))
+  if (toDate) query.set('to_time', toDate.toISOString().slice(0, 10))
+  return await fetch(
+    `${erpPath}api/method/manictime_integration.api.app.get_user_usage_and_activities?${query.toString()}`,
+  )
+    .then((r) => r.json())
+    .then((r) => r as GetActivitiesResponse)
+}
+
+export const createActivity = async (
+  newActivity: NewActivityDto,
+  syncId: string,
+  timelineId: string,
+) =>
+  await fetch(`${erpPath}api/method/manictime_integration.api.app.create_activity`, {
+    body: JSON.stringify({ expectedLastChangeId: syncId, values: newActivity, timelineId }),
+  })
+    .then((r) => r.json())
+    .then((r) => r as GetActivitiesResponse)
+
+export const updateActivity = async (
+  newActivity: NewActivityDto,
+  syncId: string,
+  timelineId: string,
+  activityId: number,
+) =>
+  await fetch(`${erpPath}api/method/manictime_integration.api.app.create_activity`, {
+    body: JSON.stringify({
+      expectedLastChangeId: syncId,
+      values: newActivity,
+      timelineId,
+      activityId,
+    }),
+  })
+    .then((r) => r.json())
+    .then((r) => r as GetActivitiesResponse)
 
 type NewActivityDto = {
   name: string
@@ -119,19 +64,6 @@ type NewActivityDto = {
     duration: number
   }
 }
-
-export type AppSecret = {
-  apiKey: string
-  apiSecret: string
-}
-export type PasswordAuth = {
-  usr: string
-  pwd: string
-}
-export type AuthVariant = PasswordAuth | AppSecret
-
-export const [getTags, getActivities, getActivitiesAndUsage, createActivity, updateActivity] =
-  setupErpMetods(erpPath, auth)
 
 export type GetTagsResponse = {
   message: TaskDto[]
@@ -175,10 +107,4 @@ type Values = {
 type TimeInterval = {
   start: string // Date
   duration: number
-}
-
-type AuthenticateResponse = {
-  home_page: string
-  full_name: string
-  message: string
 }
